9a87eba46dd3458de7da3cac171a200c7c5e29c5
gloire <nnanggloirejb@gmail.com>
2025-11-30 05:29:23 +0545

    ajout

--- employe/views.py
+++ employe/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
+from .models import Employe # POUR IMPORTER LE MODELE EMPLOYE SI Y AVAIT PLUSIEURS ON DEVAIT METTRE * à la place de EMPLOYE
+
 # Create your views here.
+
+def liste_employes(request):
+    employes = Employe.objects.all()# renvoyer tout les employés dans notre liste
+    return render(request, 'employe/list.html' , {'employes': employes})
